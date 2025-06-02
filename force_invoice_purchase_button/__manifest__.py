11e3223d81d26eb46825ce2c4e9eb0b9c9293524
WS Consultores SpA <contacto.wsconsultores@gmail.com>
2025-06-02 11:24:23 -0400

    Update __manifest__.py

--- force_invoice_purchase_button/__manifest__.py
+++ force_invoice_purchase_button/__manifest__.py
@@ -1,14 +1,14 @@
 {
     "name": "force_invoice_purchase_button",
     "summary": "Agrega acción masiva para forzar estado facturado sin bucles prohibidos.",
-    "description": """
+    "description": "
         <p>Este módulo agrega un botón para cambiar manualmente el estado de facturación de una orden de compra a "Facturado", incluso si no tiene factura.</p>
         <ul>
             <li>No modifica la contabilidad.</li>
             <li>Sólo cambia el estado visual.</li>
             <li>Compatible con Odoo Online y Odoo.sh</li>
         </ul>
-    """,
+    ",
     "author": "WS Consultores SpA",
     "website": "https://github.com/WSConsultores",
     "license": "LGPL-3",
